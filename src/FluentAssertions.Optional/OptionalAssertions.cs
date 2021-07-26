d4b1ad24628a6d0f6776acb7c89422da07c7cde2
Søren Guldmund <sgu@immeo.dk>
2021-07-26 15:48:51 +0200

    Extract OptionalGuidAssertions to separate file

--- src/FluentAssertions.Optional/OptionalAssertions.cs
+++ src/FluentAssertions.Optional/OptionalAssertions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FluentAssertions.Primitives;
+﻿using FluentAssertions.Primitives;
 using Optional;
 using Optional.Unsafe;
 
@@ -8,13 +7,6 @@ namespace FluentAssertions.Optional
     public class OptionalAssertions<T> : OptionContinuedAssertions<T, OptionalAssertions<T>, ObjectAssertions>
     {
         public OptionalAssertions(Option<T> subject) : base(subject, new ObjectAssertions(subject.ValueOrDefault()))
-        {
-        }
-    }
-
-    public class OptionalGuidAssertions : OptionContinuedAssertions<Guid, OptionalGuidAssertions, GuidAssertions>
-    {
-        public OptionalGuidAssertions(Option<Guid> subject) : base(subject, new GuidAssertions(subject.ValueOrDefault()))
         {
         }
     }
---
+++ src/FluentAssertions.Optional/OptionalGuidAssertions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FluentAssertions.Primitives;
+using Optional;
+using Optional.Unsafe;
+
+namespace FluentAssertions.Optional
+{
+    public class OptionalGuidAssertions : OptionContinuedAssertions<Guid, OptionalGuidAssertions, GuidAssertions>
+    {
+        public OptionalGuidAssertions(Option<Guid> subject) : base(subject, new GuidAssertions(subject.ValueOrDefault()))
+        {
+        }
+    }
+}
