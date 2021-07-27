3ca756a47cc1ac9862f9813da2b6eac3303c7e65
Søren Palmund <palmund@gmail.com>
2021-07-27 09:33:10 +0200

    Move OptionalGuidAssertions to FluentAssertions.Optional.Primitives namespace

--- src/FluentAssertions.Optional/OptionExtensions.cs
+++ src/FluentAssertions.Optional/OptionExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using FluentAssertions.Optional;
 using FluentAssertions.Optional.Numeric;
+using FluentAssertions.Optional.Primitives;
 using Optional;
 
 // ReSharper disable once CheckNamespace
