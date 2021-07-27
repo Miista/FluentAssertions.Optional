9cd59654d01f65ba07c6c885b2d7cbb5301f2415
Søren Palmund <palmund@gmail.com>
2021-07-27 10:06:13 +0200

    Add Should methods for all numeric types

--- src/FluentAssertions.Optional/OptionExtensions.cs
+++ src/FluentAssertions.Optional/OptionExtensions.cs
@@ -21,6 +21,22 @@ namespace FluentAssertions
         
         public static OptionalNumericAssertions<int> Should(this Option<int> instance) => new OptionalNumericAssertions<int>(instance);
         
+        public static OptionalNumericAssertions<uint> Should(this Option<uint> instance) => new OptionalNumericAssertions<uint>(instance);
+        
+        public static OptionalNumericAssertions<long> Should(this Option<long> instance) => new OptionalNumericAssertions<long>(instance);
+        
+        public static OptionalNumericAssertions<ulong> Should(this Option<ulong> instance) => new OptionalNumericAssertions<ulong>(instance);
+        
+        public static OptionalNumericAssertions<short> Should(this Option<short> instance) => new OptionalNumericAssertions<short>(instance);
+        
+        public static OptionalNumericAssertions<ushort> Should(this Option<ushort> instance) => new OptionalNumericAssertions<ushort>(instance);
+        
+        public static OptionalNumericAssertions<byte> Should(this Option<byte> instance) => new OptionalNumericAssertions<byte>(instance);
+        
+        public static OptionalNumericAssertions<sbyte> Should(this Option<sbyte> instance) => new OptionalNumericAssertions<sbyte>(instance);
+        
+        public static OptionalNumericAssertions<float> Should(this Option<float> instance) => new OptionalNumericAssertions<float>(instance);
+        
         public static OptionalNumericAssertions<double> Should(this Option<double> instance) => new OptionalNumericAssertions<double>(instance);
         
         public static OptionalNumericAssertions<decimal> Should(this Option<decimal> instance) => new OptionalNumericAssertions<decimal>(instance);
