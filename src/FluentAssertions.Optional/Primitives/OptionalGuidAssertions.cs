ab4f71fd5521d7373753e9d00a7c9fd504c323e6
Søren Palmund <palmund@gmail.com>
2024-01-07 18:45:23 +0100

    Clean up some assertions

--- src/FluentAssertions.Optional/Primitives/OptionalGuidAssertions.cs
+++ src/FluentAssertions.Optional/Primitives/OptionalGuidAssertions.cs
@@ -16,45 +16,4 @@ namespace FluentAssertions.Optional.Primitives
             Subject = value;
         }
     }
-
-    /*
-    public class OptionalGuidAssertions : OptionContinuedAssertions<Guid, OptionalGuidAssertions, GuidAssertions>
-    {
-        public OptionalGuidAssertions(Option<Guid> subject) : base(subject, new GuidAssertions(subject.ValueOrDefault()))
-        {
-        }
-        
-        [CustomAssertion]
-        public AndConstraint<GuidAssertions> BeEmpty(
-            string because = "",
-            params object[] becauseArgs) =>
-            HaveValueAnd().BeEmpty(because, becauseArgs);
-        
-        [CustomAssertion]
-        public AndConstraint<GuidAssertions> NotBeEmpty(
-            string because = "",
-            params object[] becauseArgs) =>
-            HaveValueAnd().NotBeEmpty(because, becauseArgs);
-        
-        [CustomAssertion]
-        public AndConstraint<GuidAssertions> Be(
-            Guid expected,
-            string because = "",
-            params object[] becauseArgs) =>
-            HaveValueAnd().Be(expected, because, becauseArgs);
-        
-        [CustomAssertion]
-        public AndConstraint<GuidAssertions> NotBe(
-            Guid unexpected,
-            string because = "",
-            params object[] becauseArgs) =>
-            HaveValueAnd().NotBe(unexpected, because, becauseArgs);
-        
-        private GuidAssertions HaveValueAnd()
-        {
-            HaveValue();
-            return new GuidAssertions(Subject.ValueOrDefault());
-        }
-    }
-*/
 }
--- src/FluentAssertions.Optional/Primitives/OptionalStringAssertions.cs
+++ src/FluentAssertions.Optional/Primitives/OptionalStringAssertions.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using FluentAssertions.Primitives;
+﻿using FluentAssertions.Primitives;
 using Optional;
 using Optional.Unsafe;
 
@@ -22,58 +21,4 @@ namespace FluentAssertions.Optional.Primitives
             Subject = value;
         }
     }
-    /*
-    public class OptionalStringAssertions : OptionContinuedAssertions<string, OptionalStringAssertions, StringAssertions>
-    {
-        public OptionalStringAssertions(Option<string> subject) : base(subject, new StringAssertions(subject.ValueOrDefault()))
-        {
-        }
-
-        [CustomAssertion]
-        public AndConstraint<StringAssertions> BeOneOf(
-            IEnumerable<string> validValues,
-            string because = "",
-            params object[] becauseArgs) =>
-            HaveValueAnd().BeOneOf(validValues, because, becauseArgs);
-
-        [CustomAssertion]
-        public AndConstraint<StringAssertions> BeEquivalentTo(
-            string expected,
-            string because = "",
-            params object[] becauseArgs) =>
-            HaveValueAnd().BeEquivalentTo(expected, because, becauseArgs);
-        
-        // public AndConstraint<StringAssertions> Be(
-        //     string expected,
-        //     string because = "",
-        //     params object[] becauseArgs)
-        // {
-        //     return HaveValue().Which.Should().Be(expected, because, becauseArgs);
-        // }
-        //
-        // [CustomAssertion]
-        // public AndConstraint<StringAssertions> StartWith(
-        //     string expected,
-        //     string because = "",
-        //     params object[] becauseArgs)
-        // {
-        //     return HaveValue().Which.Should().StartWith(expected, because, becauseArgs);
-        // }
-        //
-        // [CustomAssertion]
-        // public AndConstraint<StringAssertions> EndWith(
-        //     string expected,
-        //     string because = "",
-        //     params object[] becauseArgs)
-        // {
-        //     return HaveValue().Which.Should().EndWith(expected, because, becauseArgs);
-        // }
-        
-        private StringAssertions HaveValueAnd()
-        {
-            HaveValue();
-            return new StringAssertions(Subject.ValueOrDefault());
-        }
-    }
-    */
 }
