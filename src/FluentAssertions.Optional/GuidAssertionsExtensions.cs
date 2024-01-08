87bd7fd1edbb561bd6c7491f2799448619bae290
Søren Guldmund <sgu@immeo.dk>
2024-01-08 19:22:55 +0100

    Add extensions for Nullable/GuidAssertions

---
+++ src/FluentAssertions.Optional/GuidAssertionsExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FluentAssertions.Primitives;
+using Optional;
+using Optional.Unsafe;
+
+// ReSharper disable once CheckNamespace
+namespace FluentAssertions
+{
+    // ReSharper disable once UnusedType.Global
+    public static class GuidAssertionsExtensions
+    {
+        [CustomAssertion]
+        public static AndConstraint<GuidAssertions> Be(
+            this GuidAssertions self,
+            Option<Guid> expected,
+            string because = "",
+            params object[] becauseArgs
+        ) =>
+            self.Be(expected.ValueOrDefault(), because, becauseArgs);
+
+        [CustomAssertion]
+        public static AndConstraint<GuidAssertions> NotBe(
+            this GuidAssertions self,
+            Option<Guid> option,
+            string because = "",
+            params object[] becauseArgs
+        ) =>
+            self.NotBe(option.ValueOrDefault(), because, becauseArgs);
+    }
+}
---
+++ src/FluentAssertions.Optional/NullableGuidAssertionsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentAssertions.Primitives;
+using Optional;
+using Optional.Unsafe;
+
+namespace FluentAssertions.Optional
+{
+    public static class NullableGuidAssertionsExtensions
+    {
+        [CustomAssertion]
+        public static AndConstraint<NullableGuidAssertions> Be(
+            this NullableGuidAssertions self,
+            Option<Guid?> expected,
+            string because = "",
+            params object[] becauseArgs
+        ) =>
+            self.Be(expected.ValueOrDefault(), because, becauseArgs);
+    }
+}
