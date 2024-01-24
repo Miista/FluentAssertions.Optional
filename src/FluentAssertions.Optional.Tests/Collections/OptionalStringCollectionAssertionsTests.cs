2c7e7429aec3daeba1c95653a6aaeb32a1db526b
Søren Guldmund <sgu@immeo.dk>
2024-01-24 08:24:13 +0100

    #1 Add more tests

---
+++ src/FluentAssertions.Optional.Tests/Collections/OptionalStringCollectionAssertionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Optional;
+using Xunit;
+
+namespace FluentAssertions.Optional.Tests.Collections
+{
+    public class OptionalStringCollectionAssertionsTests
+    {
+        [Fact]
+        public void Test()
+        {
+            // Arrange
+            var collection = new[] { "Hello", "World!" } as IEnumerable<string>;
+            var option = collection.Some();
+
+            // Act
+            Action act = () => option.Should().NotBeEmpty();
+            
+            // Assert
+            act.Should().NotThrow(because: "the collection is not empty");
+        }
+    }
+}
